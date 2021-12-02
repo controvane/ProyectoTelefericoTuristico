6b9ab0b6616a24c410f5ebede1135f79b2814e49
Javier Murguia <cjmurguia@gmail.com>
2021-12-02 19:46:20 -0400

    Sigue a la persona

--- app/src/main/java/com/univalle/javiermurguia/proyectoteleferico/Fragments/InfoFragment.java
+++ app/src/main/java/com/univalle/javiermurguia/proyectoteleferico/Fragments/InfoFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.univalle.javiermurguia.proyectoteleferico.Models.Marcador;
@@ -20,12 +22,14 @@ public class InfoFragment extends Fragment {
     private TextView nombre,nombreInfo,descripcion,descripcionInfo;
     private Button cerrar;
     private Marcador marker;
+    private FragmentContainerView info;
 
     public InfoFragment() {
         // Required empty public constructor
     }
 
     private void initialize(View view){
+        this.info = view.findViewById(R.id.info);
         this.nombre = view.findViewById(R.id.textViewNombre);
         this.nombreInfo = view.findViewById(R.id.textViewNombreInfo);
         this.descripcion = view.findViewById(R.id.textViewDescripcion);
@@ -39,6 +43,11 @@ public class InfoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
         getParentFragmentManager().setFragmentResultListener("infoMarcador", this, (requestKey, result) -> loadInfo(result));
     }
 
@@ -48,6 +57,7 @@ public class InfoFragment extends Fragment {
     }
 
     public void loadInfo(Bundle info){
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,0, 1.75f);
         this.marker = (Marcador) info.getSerializable("marcador");
         this.nombreInfo.setText(this.marker.getNombre());
         this.descripcionInfo.setText(this.marker.getDescripcion());
@@ -56,14 +66,17 @@ public class InfoFragment extends Fragment {
         this.descripcion.setVisibility(View.VISIBLE);
         this.descripcionInfo.setVisibility(View.VISIBLE);
         this.cerrar.setVisibility(View.VISIBLE);
+        this.info.setLayoutParams(params);
     }
 
     public void cerrarFragment(){
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,0, 0f);
         this.nombre.setVisibility(View.INVISIBLE);
         this.nombreInfo.setVisibility(View.INVISIBLE);
         this.descripcion.setVisibility(View.INVISIBLE);
         this.descripcionInfo.setVisibility(View.INVISIBLE);
         this.cerrar.setVisibility(View.INVISIBLE);
+        this.info.setLayoutParams(params);
     }
 
     @Override
--- app/src/main/java/com/univalle/javiermurguia/proyectoteleferico/MapsActivity.java
+++ app/src/main/java/com/univalle/javiermurguia/proyectoteleferico/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.univalle.javiermurguia.proyectoteleferico;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -17,6 +18,7 @@ import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,6 +28,10 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.univalle.javiermurguia.proyectoteleferico.Models.Marcador;
 import com.univalle.javiermurguia.proyectoteleferico.databinding.ActivityMapsBinding;
 
@@ -34,11 +40,12 @@ import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    protected GoogleMap mMap;
     private ActivityMapsBinding binding;
     private List<Marcador> marcadores;
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-    private FusedLocationProviderClient myLocationProviderClient;
+    protected FusedLocationProviderClient myLocationProviderClient;
+    protected PlacesClient placesClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,6 +101,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         getMyLocation();
     }
 
+
+
     //Esta función permite cargar el fragment de información, tecnicamente el fragment ya esta ahi, pero vuelve todo visible desde el otro lado
     public boolean infoOfMarker(Marker marker){
         Marcador marcador;
@@ -126,47 +135,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     //Esta función y la siguiente clase son para poder seguir al usuario mientras se mueve
     private void getMyLocation(){
-        this.myLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        new Thread(new FollowerRunnable(this.myLocationProviderClient,this.mMap));
+        this.mMap.getUiSettings().setAllGesturesEnabled(false);
+        Places.initialize(getApplicationContext(), getString(R.string.maps_api_key));
+        this.placesClient = Places.createClient(MapsActivity.this);
+        this.myLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
+        try{
+            this.mMap.setMyLocationEnabled(true);
+            this.mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        }
+        catch(SecurityException ex){
+            Log.d("errorUbicacion",ex.getMessage()+" error de seguridad en get my Location");
+        }
+        catch(NullPointerException ex){
+            Log.d("errorUbicacion",ex.getMessage()+" error de null pointer en get my Location");
+        }
+        new Thread(new FollowerRunnable()).start();
     }
 
     private class FollowerRunnable implements Runnable{
 
-        FusedLocationProviderClient myLocation;
-        GoogleMap mMap;
-
-        public FollowerRunnable(FusedLocationProviderClient myLocation, GoogleMap mMap) {
-            this.myLocation = myLocation;
-            this.mMap = mMap;
-        }
-
         @Override
         public void run() {
-            Handler handler = new Handler(Looper.getMainLooper());
-            while(!Thread.interrupted()){
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        follower();
-                    }
-                });
-            }
+            follower();
         }
 
         private void follower(){
             try{
-                FollowerRunnable.this.myLocation.getLastLocation().addOnSuccessListener(location -> logicGetMyLocation(location));
+                Log.d("perseguir","estoy dentro del try de el follower");
+                Task<Location> locationResult = MapsActivity.this.myLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(MapsActivity.this, task -> logicGetMyLocation(task));
             }
             catch(SecurityException ex){
-                Log.d("ubicacion","hubo un error al conseguir la ubicacion actual");
+                Log.d("errorUbicacion",ex.getMessage()+" error de seguridad en follower");
             }
             catch(NullPointerException ex){
-                Log.d("ubicacion","la ubicacion se consiguio, pero hubo un error al ir a la ubicación");
+                Log.d("errorUbicacion",ex.getMessage()+" error de null pointer en follower");
             }
         }
 
-        private void logicGetMyLocation(Location location){
-            FollowerRunnable.this.mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
+        private void logicGetMyLocation(Task<Location> task){
+            Log.d("perseguir","estoy dentro de logicGetMyLocation");
+            Location location;
+            if(task.isSuccessful()){
+                location = task.getResult();
+                if(location != null){
+                    MapsActivity.this.mMap.moveCamera(CameraUpdateFactory.newLatLng(
+                            new LatLng(
+                                    location.getLatitude(),
+                                    location.getLongitude())));
+                }
+            }else {
+                Log.d("errorUbicacion", "La ubicación actual es nula. Usando predeterminada.");
+                Log.e("errorUbicacion", "Exception: %s", task.getException());
+                mMap.moveCamera(CameraUpdateFactory
+                        .newLatLngZoom(new LatLng(
+                                MapsActivity.this.marcadores.get(0).getLatitud(),
+                                MapsActivity.this.marcadores.get(0).getLongitud()
+                                ), 17));
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            }
         }
     }
 }
